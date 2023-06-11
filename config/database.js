365b062e3c32ac85b8be1daf2ea4cb2eab7f6533
unknown <erbynn1234@gmail.com>
2023-06-11 17:48:55 +0000

    db issue

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 import config from './config.js';
 
-const env = process.env.NODE_ENV || 'development';
+const env = 'development';
 const dbConfig = config[env];
 
 const sequelize = new Sequelize(
