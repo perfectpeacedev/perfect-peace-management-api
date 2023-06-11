dcfc5794ea985ba623ff6340dd60dbcf8b6c2468
unknown <erbynn1234@gmail.com>
2023-06-11 19:18:03 +0000

    hmm

--- controllers/onlyStudentsController.js
+++ controllers/onlyStudentsController.js
@@ -32,7 +32,20 @@ const fetchNurseryResults = async (req, res, next) => {
     const { id } = req.params.user;
 
     try {
-        const data = await getNurseryResults(id);
+        const results = await getNurseryResults(id);
+
+        const separateArrays = {};
+        results.forEach((result) => {
+            const key = `${result.term}_${result.class}_${result.formatted_date}`;
+            if (!separateArrays[key]) {
+                separateArrays[key] = [];
+            }
+            separateArrays[key].push(result);
+        });
+
+        // Convert the separate arrays into an array of arrays
+        const data = Object.values(separateArrays);
+
         res.json(data);
     } catch (error) {
         console.log(error)
