51589f412a623609b56ee67be7138ebdf9b173e9
Raphael Erbynn <erbynn1234@gmail.com>
2023-10-09 19:58:43 +0000

    hmm

--- services/term.js
+++ services/term.js
@@ -1,6 +1,8 @@
 import { Term } from "../models/index.js";
 
 const getTerm = async () => {
+    const test = await Term.findAll();
+    console.log(test)
     const response = await Term.findOne();
     return response;
 }
